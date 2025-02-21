8b8221ee12ea15a163d63851ff4cac241481e618
Vishal Aakash <111877553+vishalmet@users.noreply.github.com>
2025-02-21 17:56:22 +0530

    changed to local base url

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -22,7 +22,7 @@ export async function generateMetadata() {
       'twitter:site': '@winksdotfun',
       'twitter:title': ' Buy memecoins with 1-click from Twitter!',
       'twitter:description': 'Use any EVM wallet to buy your favorite memecoin from any EVM chain! Trade and refer others to earn Winks points.',
-      'twitter:player': 'https://buymemes.winks.fun',
+      'twitter:player': 'https://bnbswap-winks.vercel.app',
       'twitter:player:width': '360',
       'twitter:player:height': '560',
       'twitter:image': 'https://res.cloudinary.com/dvddnptpi/image/upload/v1739442596/rq0wjymypjel3pg6xziu.png',
